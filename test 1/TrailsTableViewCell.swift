96ab5895c4ec3e50a072109cd7ad0960768ec924
speckhalsa <sburnham3@gmail.com>
2021-02-25 22:19:25 -0500

    commit

---
+++ test 1/TrailsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TrailsTableViewCell.swift
+//  test 1
+//
+//  Created by Tick1 on 2/21/21.
+//
+
+import UIKit
+
+class TrailsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
