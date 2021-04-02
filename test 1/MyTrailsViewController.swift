58087be40cff4495f1174dad80a382f4771c6a0f
LOLHSTicks <fislerr@region18.org>
2021-04-01 20:24:33 -0400

    april 1st second commit

--- test 1/MyTrailsViewController.swift
+++ test 1/MyTrailsViewController.swift
@@ -22,6 +22,8 @@ class MyTrailsViewController: UIViewController, UITableViewDataSource {
     
     let ref = Database.database().reference(withPath: "Hikes")
     let userID = Auth.auth().currentUser!.uid
+    var array:[String]=[]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,35 +35,39 @@ class MyTrailsViewController: UIViewController, UITableViewDataSource {
                      print("Error getting data \(error)")
                  }
                  else if snapshot.exists() {
-                     print("Got All data \(snapshot.value!)")
+                     print("Got All my data \(snapshot.value!)")
+                    
+                    let enumerator = snapshot.children
+                    while let nextObj = enumerator.nextObject() as? DataSnapshot {
+                        //NSLog("current value:\(nextObj.value!)")
+                        
+                        self.array.append(nextObj.value as! String)
+                        //NSLog   ("array after appending current data :",array)
+                    }
                  }
                  else {
                      print("No data available")
                  }
              }
-
-//        let user = Auth.auth().currentUser!
-//        Auth.auth().addStateDidChangeListener { auth, user in
-//          guard let user = user else { return }
-//          self.user = User(authData: user)
         
         }
-    func readHikes() {
-    
-        ref.child(userID).observeSingleEvent(of: .value, with: { (snapshot) in
-      // Get user value
-      let value = snapshot.value as? NSDictionary
-      let username = value?["username"] as? String ?? ""
-
-      // ...
-      }) { (error) in
-        print(error.localizedDescription)
-    }
-}
+//    func readHikes() {
+//
+//        ref.child(userID).observeSingleEvent(of: .value, with: { (snapshot) in
+//      // Get user value
+//      let value = snapshot.value as? NSDictionary
+//      let username = value?["username"] as? String ?? ""
+//
+//      // ...
+//      }) { (error) in
+//        print(error.localizedDescription)
+//    }
+//}
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
  
-        return DataLoader().generatedData.count
+        return array.count
+//        return DataLoader().generatedData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -73,37 +79,19 @@ class MyTrailsViewController: UIViewController, UITableViewDataSource {
 //        return myCell
         print("MyTrailsViewController.cellForRowAt")
         
-        let user=Auth.auth().currentUser
         
-        let trailname=dataOne[indexPath.row].trailName
-        print ("\n",user!.uid," ",trailname)
+        let trailname = array[indexPath.row]
+//        print ("\n",user!.uid," ",trailname)
         
 //        let range = NSRange(location: 0, length: dataOne[indexPath.row].trailName.utf16.count)
 //        let regex = try! NSRegularExpression(pattern: "[a-z]at")
 //        regex.firstMatch(in: trailname, options: [], range: range) != nil
 
-        let replaced = trailname.replacingOccurrences(of: ".", with: "&")
+        let replaced = trailname.replacingOccurrences(of: "|", with: ".")
         
-        self.ref.child(user!.uid).child(replaced).getData { (error, snapshot) in
-            if let error = error {
-                print(replaced,":Error getting data \(error)")
-            }
-            else if snapshot.exists() {
-                print(replaced,"Got data \(snapshot.value!)")
-                
-                let value=snapshot.value as? NSDictionary
-                let dates = value?["dates"] as? String ?? ""
-                //let user = User(username: username)
-                print("dates:",dates)
-                
-            }
-            else {
-                print(replaced,"No data available")
-            }
-        }
 
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
-        cell.textLabel?.text = dataOne[indexPath.row].trailName + "   more info"
+        cell.textLabel?.text = array[indexPath.row] + "   more info"
         
         
         return cell
